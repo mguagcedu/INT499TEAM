ce8941a96a43d3df30b63764d6be4f46db60e49f
Manuel Gomez <Manuel.gomez2@student.uagc.edu>
2025-11-16 21:37:52 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,61 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext";
-import { AppProvider } from "./context/AppContext";
-import Layout from "./components/Layout";
-
-import LandingPage from "./pages/LandingPage";
-import PlannerPage from "./pages/PlannerPage";
-import MoviesPage from "./pages/MoviesPage";
-import ShopPage from "./pages/ShopPage";
-import FavoritesPage from "./pages/FavoritesPage";
+import { Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./AuthContext";
+import PrivateRoute from "./PrivateRoute";
+import Login from "./Login";
+import StreamListPage from "./pages/StreamListPage";
 import CartPage from "./pages/CartPage";
-import LoginPage from "./pages/LoginPage";
+import CheckoutPage from "./CheckoutPage";
 
-function AppRoutes() {
+const App = () => {
   return (
-    <Routes>
-      {/* Landing / splash page */}
-      <Route path="/" element={<LandingPage />} />
+    <AuthProvider>
+      <Routes>
+        {/* Public login route */}
+        <Route path="/login" element={<Login />} />
 
-      {/* All main app pages share the dashboard layout */}
-      <Route element={<Layout />}>
-        <Route path="/planner" element={<PlannerPage />} />
-        <Route path="/movies" element={<MoviesPage />} />
-        <Route path="/shop" element={<ShopPage />} />
-        <Route path="/favorites" element={<FavoritesPage />} />
-        <Route path="/cart" element={<CartPage />} />
-      </Route>
+        {/* Protected main app interface */}
+        <Route
+          path="/"
+          element={
+            <PrivateRoute>
+              <StreamListPage />
+            </PrivateRoute>
+          }
+        />
 
-      {/* Dedicated login page for real Google OAuth */}
-      <Route path="/login" element={<LoginPage />} />
+        {/* Protected cart */}
+        <Route
+          path="/cart"
+          element={
+            <PrivateRoute>
+              <CartPage />
+            </PrivateRoute>
+          }
+        />
 
-      {/* Fallback */}
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
+        {/* Protected checkout */}
+        <Route
+          path="/checkout"
+          element={
+            <PrivateRoute>
+              <CheckoutPage />
+            </PrivateRoute>
+          }
+        />
+
+        {/* Fallback */}
+        <Route
+          path="*"
+          element={
+            <PrivateRoute>
+              <StreamListPage />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
+    </AuthProvider>
   );
-}
+};
 
-export default function App() {
-  return (
-    <BrowserRouter>
-      <AuthProvider>
-        <AppProvider>
-          <AppRoutes />
-        </AppProvider>
-      </AuthProvider>
-    </BrowserRouter>
-  );
-}
+export default App;
